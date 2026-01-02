f8220b780ab2779187ae4d06910dedab31160476
Masatake Komine <minewo@DESKTOP-A1US1UP.localdomain>
2026-01-02 17:30:59 +0900

    build: react-native-worklets のAndroid自動リンクを無効化

--- react-native.config.js
+++ react-native.config.js
@@ -1,8 +1,8 @@
 module.exports = {
   dependencies: {
-    'react-native-worklets-core': {
+    'react-native-worklets': {
       platforms: {
-        android: null, // Disable autolinking on Android to avoid build errors with RN 0.76
+        android: null, // Disable autolinking on Android to avoid build errors with RN 0.76+
         ios: null,     // Disable autolinking on iOS as well
       },
     },
