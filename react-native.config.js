10b0ff008ecaa310941acd1bd867a2b78db82d61
Masatake Komine <minewo@DESKTOP-A1US1UP.localdomain>
2026-01-02 18:40:55 +0900

    fix: 両方のworkletsパッケージを無効化 (RN 0.76互換性修正)

--- react-native.config.js
+++ react-native.config.js
@@ -1,9 +1,17 @@
 module.exports = {
   dependencies: {
+    // Both worklets packages need to be disabled for RN 0.76+ compatibility
+    // They are only used by NativeWind for web builds (Babel plugin)
+    'react-native-worklets': {
+      platforms: {
+        android: null,
+        ios: null,
+      },
+    },
     'react-native-worklets-core': {
       platforms: {
-        android: null, // Disable autolinking on Android to avoid build errors with RN 0.76+
-        ios: null,     // Disable autolinking on iOS as well
+        android: null,
+        ios: null,
       },
     },
   },
