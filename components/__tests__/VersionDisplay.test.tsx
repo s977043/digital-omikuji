4624ca3b42e5a99db99872bf1ee7644faf441852
Masatake Komine <minewo@DESKTOP-A1US1UP.localdomain>
2026-01-04 13:55:28 +0900

    test: achieve 80% coverage (Issue #121)

---
+++ components/__tests__/VersionDisplay.test.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { render } from "@testing-library/react-native";
+import { VersionDisplay } from "../VersionDisplay";
+
+jest.mock("../../utils/VersionInfo", () => ({
+    getVersionDisplay: () => "v1.0.0",
+}));
+
+describe("VersionDisplay", () => {
+    it("renders version", () => {
+        const { getByText } = render(<VersionDisplay />);
+        expect(getByText("v1.0.0")).toBeTruthy();
+    });
+});
--- jest.setup.js
+++ jest.setup.js
@@ -1,5 +1,12 @@
 import "@testing-library/jest-native/extend-expect";
 
+// Mock react-native-reanimated
+jest.mock("react-native-reanimated", () => {
+  const Reanimated = require("react-native-reanimated/mock");
+  Reanimated.default.call = () => { };
+  return Reanimated;
+});
+
 // Mock expo-haptics
 jest.mock("expo-haptics", () => ({
   impactAsync: jest.fn(),
