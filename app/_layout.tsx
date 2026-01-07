ee81accc7559cb988b4ae92de30e31b74c05ed4b
Masatake Komine <minewo@DESKTOP-A1US1UP.localdomain>
2026-01-08 02:10:35 +0900

    style: fix Prettier formatting in sentry.ts

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,7 +3,6 @@ import "react-native-get-random-values";
 
 // Sentry初期化 (エラー監視) - 早期に実行する必要あり
 import { initializeSentry, Sentry } from "../utils/sentry";
-initializeSentry();
 
 import { SplashScreen, Stack } from "expo-router";
 import {
@@ -14,7 +13,8 @@ import {
 import { useEffect } from "react";
 import { logVersionInfo } from "../utils/VersionInfo";
 import "../global.css";
-import "../i18n"; // i18n initialization
+import "../i18n";
+initializeSentry(); // i18n initialization
 
 // スプラッシュスクリーンを自動的に隠さないように設定
 SplashScreen.preventAutoHideAsync();
