9c4ef9d2730fede5b5af3f9fa967e5a9180cb1c4
Masatake Komine <minewo@DESKTOP-A1US1UP.localdomain>
2026-01-09 23:39:09 +0900

    refactor(ui): simplify VersionDisplay component by removing redundant View wrapper

--- components/VersionDisplay.tsx
+++ components/VersionDisplay.tsx
@@ -1,4 +1,4 @@
-import { View, Text } from "react-native";
+import { Text } from "react-native";
 import { getVersionDisplay } from "../utils/VersionInfo";
 
 /**
@@ -6,10 +6,10 @@ import { getVersionDisplay } from "../utils/VersionInfo";
  * 画面右下に薄いテキストで表示
  */
 export const VersionDisplay = () => (
-  <View
-    className="absolute bottom-2 right-2 p-1"
+  <Text
+    className="absolute bottom-2 right-2 p-1 text-xs text-white/40 font-mono"
     accessibilityLabel={`アプリバージョン: ${getVersionDisplay()}`}
   >
-    <Text className="text-xs text-white/40 font-mono">{getVersionDisplay()}</Text>
-  </View>
+    {getVersionDisplay()}
+  </Text>
 );
